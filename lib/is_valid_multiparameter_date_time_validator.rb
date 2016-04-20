e5bec21085dfaa7e6d086080c64f6476df8da21b
Johny Ho and Mitch Saltykov <pair+Johny+mitch@casecommons.org>
2016-04-20 14:33:02 -0400

    Refactoring: extract out attribute date/time keys

--- lib/is_valid_multiparameter_date_time_validator.rb
+++ lib/is_valid_multiparameter_date_time_validator.rb
@@ -2,8 +2,11 @@ require 'active_model/validator'
 
 class IsValidMultiparameterDateTimeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    date_value = record.public_send(:"#{attribute}_date_part")
-    time_value = record.public_send(:"#{attribute}_time_part")
+    date_part_key = :"#{attribute}_date_part"
+    date_value = record.public_send(date_part_key)
+
+    time_part_key = :"#{attribute}_time_part"
+    time_value = record.public_send(time_part_key)
 
     return if !options[:required] && date_value.blank? && time_value.blank?
 
@@ -13,19 +16,16 @@ class IsValidMultiparameterDateTimeValidator < ActiveModel::EachValidator
     elsif date_invalid?(date_value) || time_invalid?(time_value)
       record.errors.add(attribute, self.class.invalid_format_error_message)
     elsif date_value.blank?
-      key = :"#{attribute}_date_part"
-      message = record.errors.generate_message(key, :blank, default: 'Please enter a date.')
+      message = record.errors.generate_message(date_part_key, :blank, default: 'Please enter a date.')
       record.errors.add(attribute, message)
     elsif time_value.blank?
-      key = :"#{attribute}_time_part"
-      message = record.errors.generate_message(key, :blank, default: 'Please enter a time.')
+      message = record.errors.generate_message(time_part_key, :blank, default: 'Please enter a time.')
       record.errors.add(attribute, message)
     else
-      attribute_value = record.public_send(:"#{attribute}_time_part")
       begin
         Date.parse(date_value)
         Time.zone.parse("#{date_value} #{time_value}")
-        Time.zone.parse(attribute_value)
+        Time.zone.parse(time_value)
       rescue ArgumentError
         record.errors.add(attribute, self.class.invalid_format_error_message)
       end
